aa7eb3c9aeb309aea00b5716d2a19be54c211849
Solofoniaina Armando <armandodapa10@gmail.com>
2025-10-30 11:28:05 +0100

    feat-drag-and-drop

--- V2Trello/app.js
+++ V2Trello/app.js
@@ -1,10 +1,10 @@
-document.addEventListener('alpine:init', () => {
-  Alpine.store('dataStore', {
-    Todo: Alpine.$persist([]).as('Todo'),
-    Doing: Alpine.$persist([]).as('Doing'),
-    Done: Alpine.$persist([]).as('Done'),
+document.addEventListener("alpine:init", () => {
+  Alpine.store("dataStore", {
+    Todo: Alpine.$persist([]).as("Todo"),
+    Doing: Alpine.$persist([]).as("Doing"),
+    Done: Alpine.$persist([]).as("Done"),
 
-    infosBtn: Alpine.$persist('true').as('infoBtn'),
+    infosBtn: Alpine.$persist("true").as("infoBtn"),
 
     todoList: true,
     doingList: true,
@@ -15,25 +15,30 @@ document.addEventListener('alpine:init', () => {
     openDone: false,
     openEdit: false,
 
-    editTaskData: { list: '', index: -1, name: '', desc: '' },
-    search: '',
+    dragList: null,
+    dragIndex: null,
+    dropList: null,
+    dropIndex: null,
+
+    editTaskData: { list: "", index: -1, name: "", desc: "" },
+    search: "",
 
     infoModal() {
       this.infosBtn = !this.infosBtn;
     },
 
     filtreList(i) {
-      if (i == 'todo') {
+      if (i == "todo") {
         this.doingList = !this.doingList;
         this.doneList = !this.doneList;
-      } else if (i == 'doing') {
+      } else if (i == "doing") {
         this.todoList = !this.todoList;
         this.doneList = !this.doneList;
-      } else if (i == 'done') {
+      } else if (i == "done") {
         this.todoList = !this.todoList;
         this.doingList = !this.doingList;
       } else {
-        alert('Il ya un problème dans la filter liste');
+        alert("Il ya un problème dans la filter liste");
       }
     },
 
@@ -73,14 +78,65 @@ document.addEventListener('alpine:init', () => {
         this[newList][this[newList].length - 1] = task;
       }
       this.openEdit = false;
-      this.editTaskData = { list: '', index: -1, name: '', desc: '' };
+      this.editTaskData = { list: "", index: -1, name: "", desc: "" };
     },
 
     handleCheckbox(listName, index) {
-      if (listName === 'Todo') this.moveTask(this.Todo, this.Doing, index);
-      else if (listName === 'Doing')
+      if (listName === "Todo") this.moveTask(this.Todo, this.Doing, index);
+      else if (listName === "Doing")
         this.moveTask(this.Doing, this.Done, index);
-      else if (listName === 'Done') this.deleteTask(this.Done, index);
+      else if (listName === "Done") this.deleteTask(this.Done, index);
+    },
+
+    startDrag(listName, index) {
+      this.dragList = listName;
+      this.dragIndex = index;
+      this.dropList = null;
+      this.dropIndex = null;
+    },
+
+    enterDrop(listName, index) {
+      this.dropList = listName;
+      this.dropIndex = index;
+    },
+
+    leaveDrop(listName, index) {
+      if (this.dropList === listName && this.dropIndex === index) {
+        this.dropList = null;
+        this.dropIndex = null;
+      }
+    },
+
+    dropItem(targetListName) {
+      if (this.dragList === null) return;
+
+      const source = this[this.dragList];
+      const target = this[targetListName];
+
+      const [item] = source.splice(this.dragIndex, 1);
+
+      let insertIndex = null;
+      if (this.dropList === targetListName && this.dropIndex !== null) {
+        insertIndex = this.dropIndex;
+      }
+
+      if (insertIndex === null || insertIndex > target.length) {
+        target.push(item);
+      } else {
+        if (this.dragList === targetListName) {
+          if (this.dragIndex < insertIndex) insertIndex = insertIndex - 1;
+        }
+        target.splice(insertIndex, 0, item);
+      }
+
+      this.endDrag();
+    },
+
+    endDrag() {
+      this.dragList = null;
+      this.dragIndex = null;
+      this.dropList = null;
+      this.dropIndex = null;
     },
   });
 });
